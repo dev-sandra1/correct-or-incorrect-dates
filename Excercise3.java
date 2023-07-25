89a04d9a524047507e8a055d13c736f37de54cdf
Sandra Patiño <sandrapatiho@gmail.com>
2023-07-24 18:43:16 -0600

    third commit

--- Excercise3.java
+++ Excercise3.java
@@ -3,8 +3,7 @@ import java.util.Scanner;
 public class Excercise3 {
     public static void main(String[] args) {
            Scanner enter = new Scanner(System.in);
-           int  num1;
-           int  num2;
+           int  num1, num2, addition, subtraction, multiplication, division;
            char operation;
 
            System.out.println("write a number: ");
@@ -15,7 +14,32 @@ public class Excercise3 {
 
            System.out.println("write the operation you want to perform: ");
             operation = enter.next().charAt(0);
+            switch(operation){
 
+              case 's' :
+              case 'S' : addition = num1 + num2;
+              System.out.println("the sum is: " + addition);
+              break;
 
+               case 'r' :
+              case 'R' : subtraction = num1 - num2;
+              System.out.println("the minus is: " + subtraction);
+              break;
+
+              case 'p' :
+              case 'P' :
+              case 'm' :
+              case 'M' : multiplication = num1 * num2;
+              System.out.println("the multiplication is: " + multiplication);
+              break;
+
+              case 'd' :
+              case 'D' :division = num1 / num2;
+              System.out.println("the division is: " + division);
+              break;
+
+              default: System.out.println("error wrong operation :<");
+            }
+            
       }
     }
